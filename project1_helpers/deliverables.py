40d93b5b9b592b95a6dfb1baf6af65a69be579b5
Chen <jc131@illinois.edu>
2024-12-18 16:35:06 -0600

    version-3

--- project1_helpers/deliverables.py
+++ project1_helpers/deliverables.py
@@ -8,115 +8,127 @@ import os
 import torch.optim as optim
 from PIL import Image
 from iou import *
-import tqdm 
+from tqdm import tqdm 
 
 class BFSDataset(Dataset):
-    def __init__(self,root_dir,scene_name, split= 'train', transform_size=(360,240)):
-        # possibly add arguments to the constructor
-        # fill in code to load all the images and annotations and assign them as attributes to the dataset
-        #now we initialize the dataset 
-        #we train the whole dataset and the split its half for train 
+    def __init__(self, root_dir, scene_name, split='train', transform_size=(1430, 950)):
         self.transform_size = transform_size
-        #use this to store the train samples 
         self.samples = []
-        self.image_dir= os.path.join(root_dir,"Images",scene_name)
-        self.annotation_dir= os.path.join(root_dir,"Annotations",scene_name)
-        #firstly we get the path of each picture and annotation
+        
+        # Get paths
+        self.image_dir = os.path.join(root_dir, "Images", scene_name)
+        self.annotation_dir = os.path.join(root_dir, "Annotations", scene_name)
+        
+        # Get all dates
         for date in os.listdir(self.image_dir):
-            date_dir= os.path.join(self.image_dir,date)
-            for timestamp in os.listdir(date_dir):
-                #since the image part is jpg and annonation part is png, we need to make sure that they are the same type 
-                if(timestamp.endswith('.jpg')):
-                    image_path= os.path.join(self.image_dir,date_dir,timestamp)
-                    #we change the type all into png
-                    ann_path= os.path.join(self.annotation_dir,date_dir,timestamp[:-4]+'.png')
-                    if(ann_path.os.path.exists()):
-                        #if the annotation exists, we append the path to the samples
-                        self.samples.append((image_path,ann_path))
-        #we sort the samples based on the name but it may be abundant
+            # Skip hidden files
+            if date.startswith('.'):
+                continue
+                
+            date_path = os.path.join(self.image_dir, date)
+            # Make sure it's a directory
+            if not os.path.isdir(date_path):
+                continue
+                
+            # Get all timestamps in this date folder
+            for timestamp in os.listdir(date_path):
+                if timestamp.startswith('.'):
+                    continue
+                    
+                if timestamp.endswith('.jpg'):
+                    # Construct full paths
+                    image_path = os.path.join(self.image_dir, date, timestamp)
+                    ann_path = os.path.join(self.annotation_dir, date, 
+                                          timestamp.replace('.jpg', '.png'))
+                    
+                    if os.path.exists(ann_path):
+                        self.samples.append((image_path, ann_path))
+        
+        # Sort samples for reproducibility
         self.samples.sort()
+        
+        # Split into train/val
         n_total = len(self.samples)
-        n_half = n_total//2
-        #we split the dataset into two parts specifically for train and validation test
-        if(split == 'train'):
-            self.samples = self.samples[:n_half]
+        n_train = n_total // 2
+        
+        if split == 'train':
+            self.samples = self.samples[:n_train]
         else:
-            self.samples = self.samples[n_half:]
-        #reduce the work time reduce the its size to half to transform the image 
-        self.transform = transforms.Compose([transforms.Resize(self.transform_size),transforms.ToTensor()])
+            self.samples = self.samples[n_train:]
+            
+        # Define transforms
+        self.transform = transforms.Compose([
+            transforms.Resize(transform_size),
+            transforms.ToTensor(),
+        ])
+
     def __len__(self):
-        # return length of the dataset, i.e. number of (image, annotation) pairs
         return len(self.samples)
-    def __getitem(self, idx):
-        # return one image, annotation pair
-        # fill in code to access an (image, annotation) pair in the dataset
-        #find the path to the the data we want 
+
+    def __getitem__(self, idx):
         image_path, ann_path = self.samples[idx]
-        #open the image then transform them into tensor
+        
+        # Load and transform image
         image = Image.open(image_path).convert('RGB')
         image = self.transform(image)
-        #open the annotation and transform it into tensor
-        annotation = Image.open(ann_path).Grayscale()  # convert to grayscale
+        
+        # Load and transform annotation
+        annotation = Image.open(ann_path).convert('L')  # grayscale
         annotation = self.transform(annotation)
-        #not sure here just for test
-        #annotation = (annotation>0.5).float()
-
+        annotation = (annotation > 0.5).float()  # binarize
+        
         return image, annotation
 
 class SegmentationModel(nn.Module):
-    #since the input is RGB image, the input channel is 3
-    #since the output is the probability of the foreground, the output channel is 1
     def __init__(self, input_channels=3, output_channels=1):
-        super().__init__(self)
-        # possibly add arguments to the constructor
-        # fill in code to declare attributes and model layers
-        #we use the autoencoder to train the model with 3 layers
-        self.encoder = nn.Sequential(nn.Conv2d(input_channels, 32, kernel_size=3, padding=1,stride=2),
-                                        nn.BatchNorm2d(32),
-                                        nn.ReLU(inplace=True),
-                                        nn.Conv2d(32, 64, kernel_size=3, padding=1,stride=2),
-                                        nn.BatchNorm2d(64),
-                                        nn.ReLU(inplace=True),
-                                        nn.Conv2d(64, 128, kernel_size=3, padding=1,stride=2),
-                                        nn.BatchNorm2d(128),
-                                        nn.ReLU(inplace=True))
-        self.decoder = nn.Sequential(nn.ConvTranspose2d(128, 64, kernel_size=3, padding=1,stride=2),
-                                        nn.BatchNorm2d(64),
-                                        nn.ReLU(inplace=True),
-                                        nn.Upsample(scale_factor=2),
-                                        nn.ConvTranspose2d(64, 32, kernel_size=3, padding=1,stride=2),
-                                        nn.BatchNorm2d(32),
-                                        nn.ReLU(inplace=True),
-                                        nn.Upsample(scale_factor=2))
+        # Changed from super().__init__(self) to super().__init__()
+        super().__init__()
+        
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Conv2d(input_channels, 32, kernel_size=3, padding=1, stride=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1, stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1, stride=2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True)
+        )
+        
+        # Decoder
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=3, padding=1, stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Upsample(scale_factor=2),
+            nn.ConvTranspose2d(64, 32, kernel_size=3, padding=1, stride=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Upsample(scale_factor=2)
+        )
+        
         self.output = nn.Conv2d(32, output_channels, kernel_size=3, padding=1)
-    def forward(self, x):
-        # fill in forward function
 
-        # your forward function will return either an (N, 1, H, W) tensor or (N, 2, H, W) depending on
-        # if you implementation has two classes (background, foreground) for output channels or just
-        # one class, i.e. probability of foreground
+    def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
         x = self.output(x)
-        return x.sigmoid()
+        return torch.sigmoid(x)
 
-def training_loop(model, train_dataloader, validation_dataloader, optimizer, criterion,n_epochs):
-    #we run this on gpu if possible
-    loss_values = []
-    train_ious = []
-    val_ious = []
-    if(torch.cuda.is_available()):
-        device = torch.device('cuda')
-    model=model.to(device)
-    #criterion=nn.BCELoss()
-    #we tried two optimizer way 
-    #optimizer=optim.Adam(model.parameters(),lr=0.001)
-    #optimizer=optim.SGD(model.parameters(),lr=0.001,momentum=0.9)
-    metrics = {'loss_values': [],'train_ious':[], 'val_ious': []}
+def training_loop(model, criterion, optimizer, n_epochs, train_loader, val_loader):
+    # Set device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    
+    metrics = {'loss_values': [], 'train_ious': [], 'val_ious': []}
+    
     for n in tqdm(range(n_epochs)):
         epoch_loss = 0
         epoch_iou = 0
-        for x_batch, y_batch in train_dataloader:
+        
+        for x_batch, y_batch in train_loader:
             x_batch, y_batch = x_batch.to(device), y_batch.to(device)
             optimizer.zero_grad()
             y_pred = model(x_batch)
@@ -125,27 +137,25 @@ def training_loop(model, train_dataloader, validation_dataloader, optimizer, cri
             optimizer.step()
             epoch_loss += loss.item()
             epoch_iou += iou(y_pred, y_batch)
-        loss_values.append(epoch_loss/len(train_dataloader))
-        train_ious.append(epoch_iou/len(train_dataloader))
-
-        val_iou=0
-        for x_batch,y_batch in validation_dataloader:
-            with torch.torch_no_grad():
+            
+        metrics['loss_values'].append(epoch_loss/len(train_loader))
+        metrics['train_ious'].append(epoch_iou/len(train_loader))
+        
+        val_iou = 0
+        for x_batch, y_batch in val_loader:
+            with torch.no_grad():
                 x_batch, y_batch = x_batch.to(device), y_batch.to(device)
                 y_pred = model(x_batch)
                 val_iou += iou(y_pred, y_batch)
-        val_ious.append(val_iou/len(validation_dataloader))
-    metrics['loss_values'] = loss_values
-    metrics['train_ious'] = train_ious
-    metrics['val_ious'] = val_ious
-    # return your trained model and any metrics you computed and would like for plotting, displaying in your report
+        metrics['val_ious'].append(val_iou/len(val_loader))
+    
     return model, metrics
 
-
+# Example usage
 if __name__ == "__main__":
     # Example for one scene
     scene_name = "Buffalo Grove at Deerfield East"
-    root_dir = "\\BFSData"  # Current directory
+    root_dir = '.\\BFSData'  # Current directory
     
     # Create datasets
     train_dataset = BFSDataset(root_dir, scene_name, split='train')
@@ -161,7 +171,7 @@ if __name__ == "__main__":
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     
     # Train model
-    trained_model, losses, train_ious, val_ious = training_loop(
+    trained_model, metrics = training_loop(
         model=model,
         criterion=criterion,
         optimizer=optimizer,
